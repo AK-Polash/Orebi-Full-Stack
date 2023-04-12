2544bc958032e1978dc1ee62e1708e9a03443746
AK-Polash <polashk199@gmail.com>
2023-04-13 02:25:44 +0600

    bcrypt package added here..

--- server/controllers/registrationController.js
+++ server/controllers/registrationController.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const User = require("../models/registrationModel");
 const nameValidation = require("../utils/nameValidation");
 const emailValidation = require("../utils/emailValidation");
@@ -29,23 +30,38 @@ const registrationController = (req, res) => {
     return;
   } else if (passwordValidation(res, password)) {
     return;
-  } else {
-    const user = new User({
-      firstName,
-      lastName,
-      email,
-      telephone,
-      addressOne,
-      addressTwo,
-      city,
-      postCode,
-      country,
-      state,
-      password,
-    });
-
-    user.save().then(() => res.send({ message: "registration successful!" }));
   }
+
+  bcrypt.hash(password, 10, async (err, hash) => {
+    if (err) return res.send({ error: "internal error" });
+
+    try {
+      const existUser = await User.find({ email: email });
+      if (existUser.length === 0) {
+        const user = new User({
+          firstName,
+          lastName,
+          email,
+          telephone,
+          addressOne,
+          addressTwo,
+          city,
+          postCode,
+          country,
+          state,
+          password: hash,
+        });
+
+        return user
+          .save()
+          .then(() => res.send({ message: "registration successful" }));
+      } else {
+        return res.send({ message: "user already exist" });
+      }
+    } catch (error) {
+      return res.send({ error: "server error" });
+    }
+  });
 };
 
 module.exports = registrationController;
