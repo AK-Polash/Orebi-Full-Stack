9ac2f93e941421675dc4978771114e18dd895026
AK-Polash <polashk199@gmail.com>
2023-05-29 23:17:10 +0600

    nothing

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,6 @@ const apiRoutes = require("./api");
 const api = process.env.BASE_URL;
 
 _.use(api, apiRoutes);
-_.get(api, (req, res) => res.send({ error: "no api found on this route" }));
+_.get(api, (req, res) => res.send({ error: "No API found on this route" }));
 
 module.exports = _;
